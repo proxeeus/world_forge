130feb937702161445a66e076ec95c76d268d89d
proxeeus <AbletonGoaTrance2110>
2018-11-26 15:38:42 +0100

    restored the Quit menu. Yay.

--- gui/spawnerdialog.py
+++ gui/spawnerdialog.py
@@ -13,7 +13,15 @@ class SpawnsFrame ( wx.Frame ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Spawns", pos = wx.DefaultPosition, size = wx.Size( 500,380 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
-		
+
+		menubar = wx.MenuBar()
+		fileMenu = wx.Menu()
+		fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+		menubar.Append(fileMenu, '&File')
+		self.SetMenuBar(menubar)
+
+		self.Bind(wx.EVT_MENU, self.OnQuit, fileItem)
+
 		bSizer1 = wx.BoxSizer( wx.VERTICAL )
 		
 		self.m_treeCtrlSpawnGroups = wx.TreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 15000,10000 ), wx.TR_DEFAULT_STYLE )
@@ -26,6 +34,9 @@ class SpawnsFrame ( wx.Frame ):
 		
 		self.Centre( wx.BOTH )
 
+
+	def OnQuit(self,e):
+		exit(0)
 
 	# Double-click on a node
 	def OnSelectSpawn(self, event):
