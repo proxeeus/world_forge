199dcc1be9477ec9cf8160e569d363b8042373d8
proxeeus <AbletonGoaTrance2110>
2018-11-27 20:55:52 +0100

    started working on fleshing-out the spawnerdialog GUI (it'll be buttfugly but functional enough)

--- gui/spawnerdialog.py
+++ gui/spawnerdialog.py
@@ -10,18 +10,53 @@ pattern = "\(([^\)]+)\)"
 class SpawnsFrame ( wx.Frame ):
 	
 	def __init__(self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Spawns", pos = wx.DefaultPosition, size = wx.Size( 500,380 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Spawns", pos = wx.DefaultPosition, size = wx.Size( 818,497 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
 		self.initmenubar()
-		bSizer1 = wx.BoxSizer( wx.VERTICAL )
-		
-		self.m_treeCtrlSpawnGroups = wx.TreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 15000,10000 ), wx.TR_DEFAULT_STYLE )
-		bSizer1.Add( self.m_treeCtrlSpawnGroups, 0, wx.ALL, 5 )
+		treeViewSizer = wx.BoxSizer( wx.VERTICAL )
+
+		# Treeview
+		self.m_treeCtrlSpawnGroups = wx.TreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 275,445 ), wx.TR_DEFAULT_STYLE )
+		# Spawngroup
+		self.m_spawnGroupNameStaticText = wx.StaticText(self, wx.ID_ANY, "Spawngroup", wx.Point(300, 9), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupNameTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "", wx.Point(303, 29), wx.Size(227, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup coords
+		self.m_spawnGroupMinXStaticText = wx.StaticText(self, wx.ID_ANY, "minX", wx.Point(300, 64), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMinXTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(345, 61), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMaxXStaticText = wx.StaticText(self, wx.ID_ANY, "maX", wx.Point(411, 64), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMaxXTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(459, 61), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMinYStaticText = wx.StaticText(self, wx.ID_ANY, "minY", wx.Point(508, 64), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMMinYTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(552, 61), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMaxYStaticText = wx.StaticText(self, wx.ID_ANY, "maxY", wx.Point(627, 66), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMaxYTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(679, 61), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup Dist
+		self.m_spawnGroupDistStaticText = wx.StaticText(self, wx.ID_ANY, "Dist", wx.Point(300, 95), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupDistTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(345, 92), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup Min Delay
+		self.m_spawnGroupMinDelayStaticText = wx.StaticText(self, wx.ID_ANY, "Min Delay", wx.Point(383, 95), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupMinDelayTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "15000", wx.Point(459, 92), wx.Size(50, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup Delay
+		self.m_spawnGroupDelayStaticText = wx.StaticText(self, wx.ID_ANY, "Delay", wx.Point(508, 95), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupDelayTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "15000", wx.Point(552, 92), wx.Size(50, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup Despawn
+		self.m_spawnGroupDespawnStaticText = wx.StaticText(self, wx.ID_ANY, "Despawn", wx.Point(603, 95), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupDespawnTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(679, 92), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup Despawn Timer
+		self.m_spawnGroupDespawnTimerStaticText = wx.StaticText(self, wx.ID_ANY, "Despawn", wx.Point(300, 126), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupDespawnTimerTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(407, 126), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup Spawn Limit
+		self.m_spawnGroupSpawnLimitStaticText = wx.StaticText(self, wx.ID_ANY, "Spawn Limit", wx.Point(465, 129), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupSpawnLimitTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(552, 126), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+		# Spawngroup Change
+		self.m_spawnGroupChanceStaticText = wx.StaticText(self, wx.ID_ANY, "Chance", wx.Point(613, 129), wx.DefaultSize, wx.TR_DEFAULT_STYLE)
+		self.m_spawnGroupChanceTextCtrl = wx.TextCtrl(self, wx.ID_ANY, "0", wx.Point(679, 126), wx.Size(32, 22), wx.TR_DEFAULT_STYLE)
+
+		treeViewSizer.Add( self.m_treeCtrlSpawnGroups, 0, wx.ALL, 5 )
 
 		self.m_treeCtrlSpawnGroups.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.OnSelectSpawn )
 		
-		self.SetSizer( bSizer1 )
+		self.SetSizer( treeViewSizer )
 		self.Layout()
 		
 		self.Centre( wx.BOTH )
